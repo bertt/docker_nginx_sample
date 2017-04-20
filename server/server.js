51c5a5b628ad204cdae1abeb1609db6dd299d604
bertt <btemme@hotmai.com>
2017-04-20 14:49:04 +0200

    add header x-forwarded-server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World root!')
+  res.send('Hello World root!' + JSON.stringify(req.headers))
 })
 
 app.get('/hoho', function (req, res) {
